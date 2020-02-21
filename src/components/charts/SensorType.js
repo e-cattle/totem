e250a96893ebb49ecf76c9666e5d1e5361ac2710
Camilo Carromeu <camilo@carromeu.com>
2020-02-21 17:27:54 -0400

    Correção na chamada do endpoint de quantitativo por tipo de sensor.

--- src/components/charts/SensorType.js
+++ src/components/charts/SensorType.js
@@ -10,31 +10,38 @@ export default {
     }
   },
   mounted () {
-    var self = this
+    this.graph()
 
-    const colors = ['#41B883', '#E46651', '#00D8FF', '#e6194b', '#3cb44b', '#ffe119', '#4363d8', '#f58231', '#911eb4', '#46f0f0', '#f032e6', '#bcf60c', '#fabebe', '#008080', '#e6beff', '#9a6324', '#fffac8', '#800000', '#aaffc3', '#808000', '#ffd8b1', '#000075', '#808080']
+    this.interval = setInterval(() => { this.graph() }, 60000)
+  },
+  methods: {
+    graph () {
+      var self = this
 
-    axios.get('http://localhost:3000/totem/count-by-type', this.config).then((response) => {
-      let labels = response.data.map(o => o.label)
-      let counts = response.data.map(o => o.total)
-      self.renderChart({
-        labels: labels,
-        datasets: [
-          {
-            backgroundColor: colors,
-            data: counts
+      const colors = ['#41B883', '#E46651', '#00D8FF', '#e6194b', '#3cb44b', '#ffe119', '#4363d8', '#f58231', '#911eb4', '#46f0f0', '#f032e6', '#bcf60c', '#fabebe', '#008080', '#e6beff', '#9a6324', '#fffac8', '#800000', '#aaffc3', '#808000', '#ffd8b1', '#000075', '#808080']
+
+      axios.get('http://localhost:3000/totem/count-by-type', this.config).then((response) => {
+        let labels = response.data.map(o => o.label)
+        let counts = response.data.map(o => o.total)
+        self.renderChart({
+          labels: labels,
+          datasets: [
+            {
+              backgroundColor: colors,
+              data: counts
+            }
+          ]
+        },
+        {
+          responsive: true,
+          maintainAspectRatio: false,
+          legend: {
+            labels: {
+              fontColor: '#ffffff'
+            }
           }
-        ]
-      },
-      {
-        responsive: true,
-        maintainAspectRatio: false,
-        legend: {
-          labels: {
-            fontColor: '#ffffff'
-          }
-        }
+        })
       })
-    })
+    }
   }
 }
